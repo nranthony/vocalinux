0b788771d8df30b258cebf8c0e87b4906c57257a
Jatin K Malik <jmalik@atlassian.com>
2026-06-11 09:40:29 -0700

    test(main): include remote API model kwarg

--- tests/test_main.py
+++ tests/test_main.py
@@ -243,6 +243,7 @@ class TestMainModule(unittest.TestCase):
                 remote_api_url="",
                 remote_api_key="",
                 remote_api_endpoint="/inference",
+                remote_api_model="whisper-1",
             )
             mock_text.assert_called_once_with(wayland_mode=True)
             mock_action_handler.assert_called_once_with(mock_text_instance)
